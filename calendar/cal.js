10c1821ae16c8d73b8ef2f1b0c3369809d730ee3
twonfi <127453854+twonfi@users.noreply.github.com>
2024-11-20 19:34:30 -0800

    clarify

--- calendar/cal.js
+++ calendar/cal.js
@@ -12,7 +12,11 @@ window.addEventListener("load", function () {
                   // DO NOT USE IN PRODUCTION I'LL THINK OF BETTER WAY
                   // (but it would probably be used anyways)
 
-                  // we can use /orca_news/cal.json if Appazur blocks the proxy's IP or if the proxy blocks us
+                  /* note:
+                   we can use /orca_news/calendar.json if Appazur blocks the proxy's IP or if the proxy blocks us
+                   it's not dynamic (script needs to be run manually) but I have a home server that can run this,
+                   or we can use gh actions (free minutes)
+                  */
                 "https://api.codetabs.com/v1/proxy?quest=elginpark.appazur.com/api/a?age=30&ua=1&v=2"
               );
               const data = await response.json();
