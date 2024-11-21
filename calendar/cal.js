c0ce3c12d18fa3b1f71661306ff7d8c344c1d1e2
twonfi <127453854+twonfi@users.noreply.github.com>
2024-11-20 19:25:34 -0800

    calendar.json, add note to cal.js about the daily mirror from Appazur

--- calendar/cal.js
+++ calendar/cal.js
@@ -12,7 +12,7 @@ window.addEventListener("load", function () {
                   // DO NOT USE IN PRODUCTION I'LL THINK OF BETTER WAY
                   // (but it would probably be used anyways)
 
-                  // we can use /orca_news/cal if Appazur blocks the proxy's IP or if the proxy blocks us
+                  // we can use /orca_news/cal.json if Appazur blocks the proxy's IP or if the proxy blocks us
                 "https://api.codetabs.com/v1/proxy?quest=elginpark.appazur.com/api/a?age=30&ua=1&v=2"
               );
               const data = await response.json();
