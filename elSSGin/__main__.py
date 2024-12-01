f8a187ca5526b72a261ecee9d66c823e652fee89
twonfi <127453854+twonfi@users.noreply.github.com>
2024-12-01 07:38:38 -0800

    remove unneeded stuff and gitignore them

--- elSSGin/__main__.py
+++ elSSGin/__main__.py
@@ -6,8 +6,6 @@ import shutil
 from datetime import datetime, timezone
 
 import jinja2
-import requests
-from markupsafe import escape
 
 print(datetime.now(timezone.utc).isoformat(), sys.version)
 
@@ -22,7 +20,7 @@ jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates/'))
 templates = {
     'departments': jinja_env.get_template('departments.html'),
     'clubs': jinja_env.get_template('clubs.html'),
-    'messages': jinja_env.get_template('orca-news/messages.html'),
+    'messages': jinja_env.get_template('editable/orca-news/messages.html'),
 }
 
 file_path = '../site'
@@ -68,48 +66,7 @@ html_text = templates['clubs'].render(
 write_file(os.path.abspath('%s/clubs.html') % file_path, html_text)
 
 
-# Orca News
-def api(endpoint: str, method: str = 'GET', return_json: bool = True) \
-        -> dict | str:
-    r_text = requests.request(
-        method, f'https://elginpark.appazur.com/api/{endpoint}', headers={
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-                          ' AppleWebKit/537.36 (KHTML, like Gecko)'
-                          ' Chrome/131.0.0.0 Safari/537.36'
-        }).text
-    if return_json:
-        return json.loads(r_text)
-    else:
-        return r_text
-
+# Orca News stubs
 # Messages
-messages = api('msg')
-
-write_file('%s/orca-news/messages.json' % file_path, json.dumps(messages))
-messages = api('msg')
-
-write_file('%s/orca-news/messages.json' % file_path, json.dumps(messages))
-
-for message in messages:
-    # noinspection PyTypeChecker
-    message['text_with_br'] = re.sub('\r?\n', '<br>',
-        message['text'])
-    # noinspection PyTypeChecker
-    message['text'] = str(escape(message['text']))
-for message in messages:
-    # noinspection PyTypeChecker
-    message['text_with_br'] = re.sub('\r?\n', '<br>',
-        message['text'])
-    # noinspection PyTypeChecker
-    message['text'] = str(escape(message['text']))
-
-html_text = templates['messages'].render(
-    messages=messages,
-    last_update=datetime.now(timezone.utc).strftime('%A, %B %-d, %Y'
-                                                    ' at %-I:%M:%S %p'),
-)
+html_text = templates['messages'].render()
 write_file('%s/orca-news/messages.html' % file_path, html_text)
-
-# Calendar
-calendar = api('a')
-write_file('%s/orca-news/calendar.json' % file_path, json.dumps(calendar))
