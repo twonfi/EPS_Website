752f3c0360b5ecc73209ae7172149650e6a1e5e0
twonfi <127453854+twonfi@users.noreply.github.com>
2024-12-14 10:30:37 -0800

    show different background for block orders

--- site/resources/scripts/orca-news/calendar_data.js
+++ site/resources/scripts/orca-news/calendar_data.js
@@ -7,12 +7,19 @@ document.addEventListener('DOMContentLoaded', function() {
       center: 'title',
       right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
     },
+    navLinks: true,
+    eventColor: '#623C5B',
     validRange: {
       // only show 30 days back and 12 months forward
       start: new Date(new Date().setDate(new Date().getDate() - 30)),
       end: new Date(new Date().setDate(new Date().getDate() + 365))
     },
-    eventColor: '#623C5B',
+    nowIndicator: true,
+    // businessHours: {
+    //   daysOfWeek: [1, 2, 3, 4, 5],
+    //   startTime: '08:30',
+    //   endTime: '16:43'
+    // },
 
     events: async function (fetchInfo, successCallback, failureCallback) {
         try {
@@ -24,8 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
           const events = data.map((event) => {
             return {
               title: event.name,
-              start:
-                event.date + (event.start24 ? "T" + event.start24 : ""),
+              start: event.date + (event.start24 ? "T" + event.start24 : ""),
               end: event.end ? event.date + "T" + event.end : null,
             };
           });
@@ -36,6 +42,14 @@ document.addEventListener('DOMContentLoaded', function() {
           alert("Events could not be loaded. Please try again later.");
           failureCallback(error);
         }},
+    eventClassNames: function(arg) {
+      if (arg.event['title'] === "A-B-C-D" ||
+          arg.event['title'] === "B-A-D-C" ||
+          arg.event['title'] === "C-D-A-B" ||
+          arg.event['title'] === "D-C-B-A") {
+        return [ 'block-order' ]
+      }
+    }
   });
   calendar.render();
 
