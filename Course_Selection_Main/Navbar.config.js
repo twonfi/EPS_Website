bc3cab858b45e116aee145fb04650c51c7a2ec2a
Oliver <117231543+BeijingCornDealer@users.noreply.github.com>
2025-03-04 20:22:03 -0800

    Delete Course_Selection_Main directory
    Will upload a proper version of this dir soon

--- Course_Selection_Main/Navbar.config.js
+++
@@ -1,9 +0,0 @@
-function navbarExport(BANNER, NAVBAR) {
-  if (window.scrollY > BANNER.offsetTop + BANNER.offsetHeight) {
-    NAVBAR.className = "navbarActive";
-  } else if (window.scrollY < BANNER.offsetTop + BANNER.offsetHeight) {
-    NAVBAR.className = "navbar";
-  }
-}
-
-export { navbarExport };
--- Course_Selection_Main/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Pragma: no-cache");
-header("Expires: Thu, 01 Jan 1970 00:00:00 GMT");//always expired, so that chrome calms down on its caching
-include("home.html");
-?>
