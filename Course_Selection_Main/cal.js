73697ad99c29762a64ad6ce68767a5afe382c8b7
Erix-glitch <71719838+Erix-glitch@users.noreply.github.com>
2025-03-31 15:24:00 -0700

    calendar

--- Course_Selection_Main/cal.js
+++ Course_Selection_Main/cal.js
@@ -1,40 +1,40 @@
 window.addEventListener("load", function () {
-    var calendarEl = document.getElementById("calendar");
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: "dayGridMonth",
-        headerToolbar: {left: "prev,next today",
-            center: "title",
-            right: "dayGridMonth,timeGridWeek,timeGridDay",
-          },
-          events: async function (fetchInfo, successCallback, failureCallback) {
-            try {
-              const response = await fetch(
-                  // DO NOT USE IN PRODUCTION I'LL THINK OF BETTER WAY
-                  // (but it would probably be used anyways)
+  var calendarEl = document.getElementById("calendar");
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: "dayGridMonth",
+      headerToolbar: {left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
+        },
+        events: async function (fetchInfo, successCallback, failureCallback) {
+          try {
+            const response = await fetch(
+                // DO NOT USE IN PRODUCTION I'LL THINK OF BETTER WAY
+                // (but it would probably be used anyways)
 
-                  /* note:
-                   we can use /orca_news/calendar.json if Appazur blocks the proxy's IP or if the proxy blocks us
-                   it's not dynamic (script needs to be run manually) but I have a home server that can run this,
-                   or we can use gh actions (free minutes); it takes a few seconds to run so not a concern
-                  */
-                "https://api.codetabs.com/v1/proxy?quest=elginpark.appazur.com/api/a?age=30&ua=1&v=2"
-              );
-              const data = await response.json();
+                /* note:
+                 we can use /orca_news/calendar.json if Appazur blocks the proxy's IP or if the proxy blocks us
+                 it's not dynamic (script needs to be run manually) but I have a home server that can run this,
+                 or we can use gh actions (free minutes); it takes a few seconds to run so not a concern
+                */
+              "https://api.codetabs.com/v1/proxy?quest=elginpark.appazur.com/api/a?age=30&ua=1&v=2"
+            );
+            const data = await response.json();
 
-              const events = data.map((event) => {
-                return {
-                  title: event.name,
-                  start:
-                    event.date + (event.start24 ? "T" + event.start24 : ""),
-                  end: event.end ? event.date + "T" + event.end : null,
-                };
-              });
+            const events = data.map((event) => {
+              return {
+                title: event.name,
+                start:
+                  event.date + (event.start24 ? "T" + event.start24 : ""),
+                end: event.end ? event.date + "T" + event.end : null,
+              };
+            });
 
-              successCallback(events);
-            } catch (error) {
-              console.error("Error fetching events:", error);
-              failureCallback(error);
-            }},
-    });
-    calendar.render();
+            successCallback(events);
+          } catch (error) {
+            console.error("Error fetching events:", error);
+            failureCallback(error);
+          }},
+  });
+  calendar.render();
 });
